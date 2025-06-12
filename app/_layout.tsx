7befa1d9a1dbd1a1f5df4fff2d7ca1d8c90f940c
Dryha Andriy <driga.and@gmail.com>
2025-06-12 18:15:41 +0300

    add animations

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,17 @@
 import { Stack } from "expo-router";
 
-export default function RootLayout() {
-  return <Stack />;
+import "../global.css";
+export default function Layout() {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{ title: "Orders" }}
+      />
+      <Stack.Screen
+        name="orders/[id]"
+        options={{ title: "Order details" }}
+      />
+    </Stack>
+  );
 }
